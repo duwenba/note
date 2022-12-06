685abbb63af03634a091050d877236b4cbaa958c
duwenba <2275317692@qq.com>
2022-12-06 09:30:54 +0800

    2022-12-6 09:30:52

--- py/lable.py
+++
@@ -1,9 +0,0 @@
-from PyQt6 import QtWidgets
-import sys
-import win32con
-win32con.SW_SHOW
-app = QtWidgets.QApplication(sys.argv)
-lab = QtWidgets.QLabel('hello world')
-print(lab.windowType())
-lab.show()
-sys.exit(app.exec())
